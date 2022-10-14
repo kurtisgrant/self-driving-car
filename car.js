0b019ec0a28b92e3041f36a56c5b28fff0f77e0a
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-10-13 20:05:31 -0400

    Add ability to read from sensors

--- car.js
+++ car.js
@@ -32,9 +32,9 @@ class Car {
     this.sensor.draw(ctx);
   }
 
-  update() {
+  update(roadBorders) {
     this.#move();
-    this.sensor.update();
+    this.sensor.update(roadBorders);
   }
 
   #move() {
